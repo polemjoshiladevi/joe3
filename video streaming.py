2873dba8f1cc57accd362f2efb94d3140507c6f5
polemjoshiladevi <polemjoshila@gmail.com>
2018-07-06 14:09:33 +0530

    project

---
+++ video streaming.py
@@ -0,0 +1,18 @@
+#!C:\python\python.exe
+
+print("Content-type:text/html")
+print()
+print("<html>")
+print("<head>")
+print("</head>")
+print("<body>")
+print("<h1>")
+
+print("welcome<br>")
+print("this is iframe tag in python file to stream youtube video<br>")
+print("Enjoy the video<br>")
+print("</h1>")
+print("<iframe width='560' height='315' src='https://www.youtube.com/embed/jmcyYyqBIOQ' frameborder='0' allow='autoplay; encrypted-media' allowfullscreen></iframe>")
+
+print("</body>")
+print("</html>")
